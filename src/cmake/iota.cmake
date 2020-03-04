b6ccf83a66104cccceafff4ffd70a27b5606e84e
MatthewDarnell <matthew.darnell@protonmail.com>
2020-03-03 20:48:36 -0500

    iota.c library building on Unix

--- src/cmake/iota.cmake
+++ src/cmake/iota.cmake
@@ -11,12 +11,19 @@ endif (WIN32)
 set(LINK_FLAGS " -fPIC -Wl,--export-all-symbols")
 
 
-ExternalProject_Add(iota.c-library
-        GIT_REPOSITORY https://github.com/iotaledger/iota.c
-        GIT_TAG 22b52d7c4333d6828ab48b811807be8081fbf15c
-        UPDATE_COMMAND ""
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE="Debug" -DBUILD_SHARED_LIBS=OFF -DCMAKE_LINK_LIBRARY_FLAG=" ${CMAKE_LINK_LIBRARY_FLAG} -fPIC -lm wldap32 ws2_32 wsock32 crypt32" -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/deps
-        )
-
-
-
+if (WIN32)
+    ExternalProject_Add(iota.c-library
+            GIT_REPOSITORY https://github.com/iotaledger/iota.c
+            GIT_TAG 22b52d7c4333d6828ab48b811807be8081fbf15c
+            UPDATE_COMMAND ""
+            CMAKE_ARGS -DCMAKE_BUILD_TYPE="Debug" -DBUILD_SHARED_LIBS=OFF -DCMAKE_LINK_LIBRARY_FLAG=" ${CMAKE_LINK_LIBRARY_FLAG} -fPIC -lm wldap32 ws2_32 wsock32 crypt32" -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/deps
+            )
+else ()
+    ExternalProject_Add(iota.c-library
+            GIT_REPOSITORY https://github.com/iotaledger/iota.c
+            GIT_TAG 22b52d7c4333d6828ab48b811807be8081fbf15c
+            UPDATE_COMMAND ""
+            PREFIX ./cclient
+            CMAKE_ARGS -DCMAKE_BUILD_TYPE="Debug" -DBUILD_SHARED_LIBS=OFF -DCMAKE_LINK_LIBRARY_FLAG=" ${CMAKE_LINK_LIBRARY_FLAG} -fPIC -lm" -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/deps
+            )
+endif()
