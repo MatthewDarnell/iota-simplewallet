14eb99eacdb8058de6020a23731230c9313374cf
Matthew <matthew.d@mercuriex.com>
2020-03-06 23:12:15 -0500

    Updated header comments

--- src/iota-simplewallet.h
+++ src/iota-simplewallet.h
@@ -7,19 +7,25 @@
 #include <stdint.h>
 #include <sqlite3.h>
 
-//Initialization/Shutdown Functions
+/*
+ *
+ *   Initialization/Shutdown Functions
+ *
+*/
+
 void init_logger();
 int init_db();
 int init_crypto();
 void init_iota();
 
-
 void shutdown_iota();
 void shutdown_config();
 
+
+
 /*
  *
- *   Start/Stop the IOTA Threads
+ *   Start/Stop the IOTA Wallet Threads
  *
 */
 
@@ -54,7 +60,7 @@ void _log(enum LOG_LEVEL, const char* format, ...);
 */
 
 //Load  initial config  from  file path
-//@path NULL for defaults, file path for  json  file
+//@path NULL for defaults, file path for json config file. If @path does not exist, a new file with default settings will be created
 //returns 0 for success, -1 for  failure
 int load_config(const char *path);
 
@@ -62,7 +68,7 @@ int load_config(const char *path);
 void shutdown_config();
 
 //Get Current  Configuration Value
-//returns NULL if not found, otherwise  remember to free!
+//returns NULL if not found, otherwise remember to free
 char* get_config(const char *key);
 
 //Set  Configuration Value
@@ -77,16 +83,35 @@ int set_config(const char* key, const char* value, int8_t save);
  *
 */
 
+//Set the main account to use this @username
+int switch_account(const char* username);
+
+//Get a json string of the currently set main account
+char* get_main_account();
+
+
+//Create an account, should use one of the following #define functions
 int __create_account(const char* username, char* password, const char* imported_seed);
+
+//Create a New IOTA account
+//@username a Unique name
+//@password a password
 #define create_account(username, password) __create_account(username, password, NULL)
+
+//Import an existing IOTA seed into a simplewallet account
+//@seed an 81 character IOTA seed
 #define import_account(username, password, seed) __create_account(username, password, seed)
+
+
+//Get all stored accounts
 char* get_accounts();
 
+//Verify that the username and password are valid
+//@username: NULL to use main account
 //@zero_password: if > 0, This will mem-zero the decrypted password and seed after verifying the password is correct
 int verify_login(const char* username, char* password, int zero_password);
 
-int switch_account(const char* username);
-char* get_main_account();
+
 
 /*
  *
@@ -94,6 +119,7 @@ char* get_main_account();
  *
 */
 
+//Get a new Deposit Address
 //@username: NULL to use main account
 char* get_new_address(char* username);
 
@@ -103,13 +129,23 @@ char* get_new_address(char* username);
  *   Transaction functions
  *
 */
+
+//Get a received transaction
+//@hash: transaction hash
 char* get_incoming_transaction_by_hash(char* hash);
 
+//Get received transactions
 //@username: NULL to use main account
+//@offset: the 0-based offset at which to start looking
+//@num: the max count of transactions to return
 char* get_incoming_transactions(char* username, int offset, int num);
 
-//Returns 0 on success, < 1 if failure
+//Creates a new transaction to send
 //@username: NULL to use main account
+//@password: the password of the account
+//@dest_address: the receiving address
+//@value: the amount to send
+//Returns 0 on success, < 1 if failure
 int create_transaction(char* username, char* password, char* dest_address, uint64_t value);
 
 
