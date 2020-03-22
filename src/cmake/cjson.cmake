80c3b2f5e2dca319d37629843b86862e035ca49d
Khrystya Barchyshyn <yurii.oleksyshyn@gmail.com>
2020-03-22 10:20:37 +0200

    Removed curl. Reorganized deps dir. Changed how dependencies are fetched

--- src/cmake/cjson.cmake
+++ src/cmake/cjson.cmake
@@ -1,13 +1,16 @@
 
 #cJSON
-ExternalProject_Add(json
-        GIT_REPOSITORY https://github.com/DaveGamble/cJSON
-        GIT_TAG 2955fe5ec49a672848a323a7c943c9525d16c83f
-        BUILD_IN_SOURCE 1
-        BUILD_ALWAYS 1
-        PATCH_COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/deps/include/cjson/
-        CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ./cJSON.c ${CMAKE_SOURCE_DIR}/deps/cJSON.c
-        UPDATE_COMMAND ${CMAKE_COMMAND} -E copy ./cJSON.h ${CMAKE_SOURCE_DIR}/deps/include/cjson/
-        BUILD_COMMAND ""
-        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ./cJSON.h ${CMAKE_SOURCE_DIR}/deps/
-        )
+
+
+FetchContent_Declare(
+  cjson
+  GIT_REPOSITORY https://github.com/DaveGamble/cJSON
+  GIT_TAG 2955fe5ec49a672848a323a7c943c9525d16c83f
+)       
+
+FetchContent_GetProperties(cjson)
+if(NOT cjson_POPULATED)
+  FetchContent_Populate(cjson)
+  file(COPY ${cjson_SOURCE_DIR}/cJSON.c DESTINATION ${DEPS_DIR})
+  file(COPY ${cjson_SOURCE_DIR}/cJSON.h DESTINATION ${DEPS_DIR}/include/cjson)
+endif()
--- src/cmake/sqlite3.cmake
+++ src/cmake/sqlite3.cmake
@@ -1,11 +1,15 @@
 
 #sqlite3
-ExternalProject_Add(sqlite
-        URL  https://sqlite.org/2020/sqlite-amalgamation-3310100.zip
-        URL_HASH SHA1=a58e91a39b7b4ab720dbc843c201fb6a18eaf32b
-        BUILD_IN_SOURCE 1
-        BUILD_ALWAYS 1
-        CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ./sqlite3.c ${CMAKE_SOURCE_DIR}/deps/sqlite3.c
-        BUILD_COMMAND ""
-        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ./sqlite3.h ${CMAKE_SOURCE_DIR}/deps/include/sqlite3.h
-        )
+
+FetchContent_Declare(
+  sqlite
+  URL  https://sqlite.org/2020/sqlite-amalgamation-3310100.zip
+  URL_HASH SHA1=a58e91a39b7b4ab720dbc843c201fb6a18eaf32b
+)	
+
+FetchContent_GetProperties(sqlite)
+if(NOT sqlite_POPULATED)
+  FetchContent_Populate(sqlite)
+  file(COPY ${sqlite_SOURCE_DIR}/sqlite3.c DESTINATION ${DEPS_DIR})
+  file(COPY ${sqlite_SOURCE_DIR}/sqlite3.h DESTINATION ${DEPS_DIR}/include)
+endif()
