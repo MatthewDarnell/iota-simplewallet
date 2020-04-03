53644128184314ccac50543baa7a3e020a28a5b1
Yura Oleksyshyn <yuraolex@gmail.com>
2020-04-03 17:16:52 +0300

    Updated tags

--- src/simplewallet/cmake/iota.cmake
+++ src/simplewallet/cmake/iota.cmake
@@ -11,15 +11,19 @@ endif (WIN32)
 if (WIN32)
     set(LINK_FLAGS " -fPIC -Wl,--export-all-symbols")
     ExternalProject_Add(iota.c-library
-            GIT_REPOSITORY https://github.com/iotaledger/iota.c
-            GIT_TAG e260967bd3a9088fa03d20c1517930e23f1de591
+            GIT_REPOSITORY https://github.com/MatthewDarnell/iota.c
+            GIT_TAG 7d6dcedb478f57199bded4664447a20482068b05
             UPDATE_COMMAND ""
-            CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_SHARED_LIBS=OFF -DCMAKE_LINK_LIBRARY_FLAG=" ${CMAKE_LINK_LIBRARY_FLAG} -fPIC -lm wldap32 ws2_32 wsock32 crypt32" -DCMAKE_INSTALL_PREFIX=${DEPS_DIR}
+            CMAKE_ARGS
+                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                -DBUILD_SHARED_LIBS=OFF
+                -DCMAKE_LINK_LIBRARY_FLAG=" ${CMAKE_LINK_LIBRARY_FLAG} -fPIC -lm wldap32 ws2_32 wsock32 crypt32"
+                -DCMAKE_INSTALL_PREFIX=${DEPS_DIR}
             )
 else ()
     ExternalProject_Add(iota.c-library
             GIT_REPOSITORY https://github.com/MatthewDarnell/iota.c.git
-            GIT_TAG 0d23c8f761b07937acec4813fc1cabb14aa2ffc8
+            GIT_TAG 7d6dcedb478f57199bded4664447a20482068b05
             CMAKE_ARGS 
 		-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} 
 		-DBUILD_SHARED_LIBS=OFF 
